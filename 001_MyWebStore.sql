0bc8141ae0e9c533759046c22ae6ce92193cd30b
olegra <ole.bjornar.granas@norkart.no>
2022-09-20 21:31:04 +0200

    fixed some minor errors in the script

--- 001_MyWebStore.sql
+++ 001_MyWebStore.sql
@@ -1,46 +1,48 @@
-CREATE DATABASE MyWebStore;
-USE MyWebStore;
+    CREATE DATABASE MyWebStore;
+    USE MyWebStore;
 
-CREATE TABLE Users(
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    username VARCHAR(30),
-    password VARCHAR(30),
-    joinedDate DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+    CREATE TABLE Users(
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        username VARCHAR(30),
+        password VARCHAR(30),
+        joinedDate DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
 
-CREATE TABLE Wares(
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    name VARCHAR(30),
-    AmountInStock INT,
-    dateAdded DATETIME DEFAULT CURRENT_TIMESTAMP,
-    productInformation VARCHAR(255),
-    productImage MEDIUMBLOB
-);
+    CREATE TABLE Wares(
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        name VARCHAR(30),
+        price DECIMAL(8,2)
+        AmountInStock INT,
+        dateAdded DATETIME DEFAULT CURRENT_TIMESTAMP,
+        productInformation VARCHAR(255),
+        productImage VARBINARY
+    );
 
-CREATE TABLE WaresCategories(
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    category VARCHAR(30)
-);
+    CREATE TABLE WaresCategories(
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        category VARCHAR(30)
+    );
 
-CREATE TABLE WaresCategoriesBindings(
-    wares_id INT,
-    waresCategories_id INT,
-    PRIMARY KEY(wares_id, waresCategories_id),
-    FOREIGN KEY(wares_id) REFERENCES Wares(id),
-    FOREIGN KEY(waresCategories_id) REFERENCES WaresCategories(id)
-);
+    CREATE TABLE WaresCategoriesBindings(
+        wares_id INT,
+        waresCategories_id INT,
+        PRIMARY KEY(wares_id, waresCategories_id),
+        FOREIGN KEY(wares_id) REFERENCES Wares(id),
+        FOREIGN KEY(waresCategories_id) REFERENCES WaresCategories(id)
+    );
 
-CREATE TABLE Orders(
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    user_id INT REFERENCES Users(id),
-    orderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    hasArrived BIT,
-    hasBeenPayed BIT
-);
+    CREATE TABLE Orders(
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        user_id INT REFERENCES Users(id),
+        orderDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+        hasArrived BIT,
+        hasBeenPayed BIT
+    );
 
-CREATE TABLE Orderlists(
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    order_id FOREIGN KEY REFERENCES Orders(id),
-    wares_id FOREIGN KEY REFERENCES Wares(id),
-    amountOrdered INT
-);
+    CREATE TABLE Orderlists(
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        order_id INT FOREIGN KEY REFERENCES Orders(id),
+        wares_id INT FOREIGN KEY REFERENCES Wares(id),
+        amountOrdered INT
+    );
+
